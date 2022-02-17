fefb405a373e6374e26d1ab0698678737d7800e2
Dmp1987 <Dmp1987@gmail.com>
2022-02-17 13:08:54 +0100

    Hand Grab Rework
    Physics based grabbing and general hand rework

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -2,46 +2,47 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
 
 public class Hand : MonoBehaviour
 {
-    //Animation Variables
-    [SerializeField]private float animationSpeed;
-    Animator _animator;
-    private SkinnedMeshRenderer _mesh;
-    private float _triggerTarget;
-    private float _gripTarget;
-    private float _triggerCurrent;
-    private float _gripCurrent;    
-    private const string animatorGripParam = "Grip";
-    private const string animatorTriggerParam = "Trigger";
-    private static readonly int Grip = Animator.StringToHash(animatorGripParam);
-    private static readonly int Trigger = Animator.StringToHash(animatorTriggerParam);
-
-
+    [Space]
     //PHysics Move
-    [SerializeField] private GameObject followObject;
+    [SerializeField] private ActionBasedController controller;
     [SerializeField] private float followSpeed = 30f;
     [SerializeField] private float rotateSpeed = 100f;
+    [Space]
     [SerializeField] private Vector3 positionOffset;
     [SerializeField] private Vector3 rotationOffset;
-
+    [Space]
+    [SerializeField] private Transform palm;
+    [SerializeField] private float reachDistance = 0.1f, joinDistance = 0.05f;
+    [SerializeField] LayerMask grabableLayer;
+    [Space]
     private Transform _followTarget;
     private Rigidbody _body;
+
+    private bool _isGrabbing;
+    private GameObject _heldObject;
+    private Transform _grabPoint;
+    private FixedJoint _joint1, _joint2;
 
     // Start is called before the first frame update
     void Start()
     {
-        //Animation
-        _animator = GetComponent<Animator>();
-        _mesh = GetComponent<SkinnedMeshRenderer>();
-
         //Physics
-        _followTarget = followObject.transform;
+        _followTarget = controller.gameObject.transform;
         _body = GetComponent<Rigidbody>();
         _body.collisionDetectionMode = CollisionDetectionMode.Continuous;
         _body.interpolation = RigidbodyInterpolation.Interpolate;
         _body.mass = 20f;
+        _body.maxAngularVelocity = 20f;
+
+        //INput Setup
+        controller.selectAction.action.started += Grab;
+        controller.selectAction.action.canceled += Release;
 
         //teleport hands
         _body.position = _followTarget.position;
@@ -51,17 +52,15 @@ public class Hand : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        animateHand();
-        //
         physicsMove();
     }
 
     private void physicsMove()
     {
         //position
-        var positionWithOffset = _followTarget.position + positionOffset;
+        var positionWithOffset = _followTarget.TransformPoint(positionOffset);
         var distance = Vector3.Distance(_followTarget.position, transform.position);
-        _body.velocity = (_followTarget.position - transform.position).normalized * (followSpeed * distance);
+        _body.velocity = (positionWithOffset - transform.position).normalized * (followSpeed * distance);
 
         //rotation
         var rotationWithOffset = _followTarget.rotation * Quaternion.Euler(rotationOffset);
@@ -70,28 +69,105 @@ public class Hand : MonoBehaviour
         _body.angularVelocity = axis * (angle * Mathf.Deg2Rad * rotateSpeed);
     }
 
-    internal void setGrip(float v)
+    private void Grab(InputAction.CallbackContext context)
     {
-        _gripTarget = v;
-    }
+        Debug.Log("DING!");
+        if (_isGrabbing || _heldObject) return;
 
-    internal void setTrigger(float v)
-    {
-        _triggerTarget = v;
-    }
+        Collider[] grabableColliders = Physics.OverlapSphere(palm.position, reachDistance, grabableLayer);
+        if (grabableColliders.Length < 1) return;
 
-    void animateHand() 
-    {
-        if (_gripCurrent != _gripTarget)
+        var objectToGrab = grabableColliders[0].transform.gameObject;
+
+        var objectBody = objectToGrab.GetComponent<Rigidbody>();
+
+        if (objectBody != null)
         {
-            _gripCurrent = Mathf.MoveTowards(_gripCurrent, _gripTarget, Time.deltaTime * animationSpeed);
-            _animator.SetFloat(animatorGripParam, _gripCurrent);
+            _heldObject = objectBody.gameObject;
         }
+        else
+        {
+            objectBody = objectToGrab.GetComponentInParent<Rigidbody>();
+            if (objectBody != null)
+            {
+                _heldObject = objectBody.gameObject;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        StartCoroutine(GrabObject(grabableColliders[0], objectBody));
+    }
+
+    private IEnumerator GrabObject(Collider collider, Rigidbody targetBody)
+    {
+        _isGrabbing = true;
+
+        _grabPoint = new GameObject().transform;
+        _grabPoint.position = collider.ClosestPoint(palm.position);
+        _grabPoint.parent = _heldObject.transform;
+
+        _followTarget = _grabPoint;
+
+        //vent på at nå grabpoint
+        while (_grabPoint != null && Vector3.Distance(_grabPoint.position, palm.position) > joinDistance && _isGrabbing)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        //frys
+        _body.velocity = Vector3.zero;
+        _body.angularVelocity = Vector3.zero;
+        targetBody.velocity = Vector3.zero;
+        targetBody.angularVelocity = Vector3.zero;
+        targetBody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        targetBody.interpolation = RigidbodyInterpolation.Interpolate;
+
+        //joints
+        _joint1 = gameObject.AddComponent<FixedJoint>();
+        _joint1.connectedBody = targetBody;
+        _joint1.breakForce = float.PositiveInfinity;
+        _joint1.breakTorque = float.PositiveInfinity;
+
+        _joint1.connectedMassScale = 1;
+        _joint1.massScale = 1;
+        _joint1.enableCollision = false;
+        _joint1.enablePreprocessing = false;        
         
-        if (_triggerCurrent != _triggerTarget)
+        _joint2 = _heldObject.AddComponent<FixedJoint>();
+        _joint2.connectedBody = _body;
+        _joint2.breakForce = float.PositiveInfinity;
+        _joint2.breakTorque = float.PositiveInfinity;
+              
+        _joint2.connectedMassScale = 1;
+        _joint2.massScale = 1;
+        _joint2.enableCollision = false;
+        _joint2.enablePreprocessing = false;
+
+        //reset follow target
+        _followTarget = controller.gameObject.transform;  
+    }
+
+    private void Release(InputAction.CallbackContext context)
+    {
+        if (_joint1 != null) Destroy(_joint1);
+        if (_joint2 != null) Destroy(_joint2);
+        if (_grabPoint != null) Destroy(_grabPoint.gameObject);
+        //et sted her er der problemer (null ref på linje 139, nok deleted / destroyed)
+        if (_heldObject != null)
         {
-            _triggerCurrent = Mathf.MoveTowards(_triggerCurrent, _triggerTarget, Time.deltaTime * animationSpeed);
-            _animator.SetFloat(animatorTriggerParam, _triggerCurrent);
+            var TargetBody = _heldObject.GetComponent<Rigidbody>();
+            TargetBody.collisionDetectionMode = CollisionDetectionMode.Discrete;
+            TargetBody.interpolation = RigidbodyInterpolation.None;
+            _heldObject = null;
         }
+
+        _isGrabbing = false;
+        _followTarget = controller.gameObject.transform;
     }
 }
+
+
+
